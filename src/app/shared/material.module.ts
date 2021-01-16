06a7cf9a89afe1c7b5d6892930080f81a20db174
PatrickPe <patrick.petzmann@gmail.com>
2021-01-16 16:41:35 +0100

    bug behoben

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -4,12 +4,15 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 const modules = [
   MatIconModule,
   MatButtonModule,
   MatMenuModule,
-  MatCardModule
+  MatCardModule,
+  MatFormFieldModule
 ];
 
 @NgModule({
